e0b56393b5a9c0d2b8bac0f8b2a414de076ba32d
wars_skynox12 <90473204+Skynox12@users.noreply.github.com>
2024-07-28 23:03:26 +0200

    TEST

--- app/src/main/java/com/example/glucideaide/MainActivity.java
+++ app/src/main/java/com/example/glucideaide/MainActivity.java
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declare the variables
     EditText etWeight, etHeight;
     Button btnCalculate;
     TextView tvResult;
@@ -23,34 +22,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the variables
         etWeight = findViewById(R.id.etWeight);
         etHeight = findViewById(R.id.etHeight);
         btnCalculate = findViewById(R.id.btnCalculate);
         tvResult = findViewById(R.id.tvResult);
 
-
-        // Set a click listener on the button
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                // Get the weight and height from the user
+
                 String weight = etWeight.getText().toString();
                 String height = etHeight.getText().toString();
 
-                // Check if the weight and height are not empty
                 if (weight.isEmpty() || height.isEmpty()) {
-                    // Display a toast message
                     Toast.makeText(MainActivity.this, "Please enter the weight and height", Toast.LENGTH_SHORT).show();
                 } else {
-                    // Calculate the BMI
                     float weightValue = Float.parseFloat(weight);
                     float heightValue = Float.parseFloat(height) / 100;
                     float bmi = weightValue / (heightValue * heightValue);
 
-                    // Display the result
-                    tvResult.setText("Your BMI is: " + bmi);
+                    tvResult.setText("Votre IMC est: " + bmi);
                 }
             }
         });
